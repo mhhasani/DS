6ceec38bf6368f5cb8f4ad5a7829730b3470fc33
mohsen <hasanimohammadhosein@gmail.com>
2021-09-30 15:06:50 +0330

    static

--- DSfriend/settings.py
+++ DSfriend/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/home/DSDrRahmani/DS/DSfriend/static'
 LOGIN_REDIRECT_URL = 'Home'
 LOGOUT_REDIRECT_URL = 'Home' 
 
