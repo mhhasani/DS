2354fe3fdcbd4285fedb060215d6da7d6820853e
mohsen <hasanimohammadhosein@gmail.com>
2021-09-30 20:40:04 +0330

    change Static_root

--- DSfriend/settings.py
+++ DSfriend/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/DSDrRahmani/DS/DSfriend/static'
+STATIC_ROOT = '/home/DSDrRahmani/DS/friend/static'
 LOGIN_REDIRECT_URL = 'Home'
 LOGOUT_REDIRECT_URL = 'Home' 
 
